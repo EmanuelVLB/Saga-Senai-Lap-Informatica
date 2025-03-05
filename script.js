25ed18e5082586eeee22c14ed567c302fced83ad
Embaeca <118907653+Embaeca@users.noreply.github.com>
2025-03-05 18:32:01 -0300

    Alteração do html css e js, (funciona) (eu acho)

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Configurando menu pequeno
 
+let bt_menu = window.document.querySelector(".menu_open_or_close");
+let menu_ativo = window.document.querySelector(".menu_ativo");
+let contador = 0;
+bt_menu.addEventListener("click", function () {
+  if (contador == 0) {
+    menu_ativo.style.display = "block";
+    contador++;
+  } else {
+    menu_ativo.style.display = "none";
+    contador -= 1;
+  }
+});
