3756c7ddaa1b090c59823610e4d095789a89785b
Embaeca <118907653+Embaeca@users.noreply.github.com>
2025-04-12 01:01:42 -0300

    .

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 // Configurando menu pequeno
-
-let bt_menu = window.document.querySelector(".menu_open_or_close");
-let menu_ativo = window.document.querySelector(".menu_ativo");
-let contador = 0;
-bt_menu.addEventListener("click", function () {
-  if (contador == 0) {
-    menu_ativo.style.display = "block";
-    contador++;
-  } else {
-    menu_ativo.style.display = "none";
-    contador -= 1;
-  }
-});
-//Tem que dar um required = true no id - data
+{
+  let bt_menu = window.document.querySelector(".--menuToggle");
+  let menu_ativo = window.document.querySelector(".--activeMenu");
+  let contador = 0;
+  
+  bt_menu.addEventListener("click", function () {
+    if (contador == 0) {
+      menu_ativo.style.display = "block";
+      contador++;
+    } else {
+      menu_ativo.style.display = "none";
+      contador -= 1;
+    }
+  });
+}
